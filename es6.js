17ce4aac1e728eaf6e8bcc358a3d8b8cf9858241
Garrett Smith <19smitgr@users.noreply.github.com>
2016-08-01 22:02:52 -0400

    Add V2

--- es6.js
+++ es6.js
@@ -1,87 +1,65 @@
-// Set up user one's circle
-
-let currentDistanceofUserOne = 50;
-let canvasOne = document.getElementById("myCanvasOne");
-let ctxOne = canvasOne.getContext("2d");
-
-function drawCircleOne(distance) {
-    ctxOne.beginPath();
-    ctxOne.arc(distance, 50, 50, 0, 2 * Math.PI);
-    ctxOne.fillStyle = "red";
-    ctxOne.fill();
-    ctxOne.stroke();
-}
-drawCircleOne(currentDistanceofUserOne);
-
-
-// Set up user two's circle
-
-let currentDistanceofUserTwo = 50;
-let canvasTwo = document.getElementById("myCanvasTwo");
-let ctxTwo = canvasTwo.getContext("2d");
-
-function drawCircleTwo(distance) {
-    ctxTwo.beginPath();
-    ctxTwo.arc(distance, 50, 50, 0, 2 * Math.PI);
-    ctxTwo.fillStyle = "red";
-    ctxTwo.fill();
-    ctxTwo.stroke();
-}
-drawCircleTwo(currentDistanceofUserTwo);
-
-
-// Manipulate circles based on trivia questions
-
-function turn(){
-    function userOne() {
-        let myNumber = Math.floor((Math.random() * 2) + 1);
-
-        let move = prompt("Am I thinking of 1 or 2?");
-        if(move == myNumber){
-            alert("Correct!");
-            currentDistanceofUserOne += 100;
-            ctxOne.clearRect(0, 0, canvasOne.width, canvasOne.height);
-            drawCircleOne(currentDistanceofUserOne);
-        } else {
-            alert("Wrong! Maybe next time. The number was " + myNumber);
-        }
+class User {
+    constructor(distance, canvas) {
+        this.distance = distance;
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+        this.win = false;
     }
 
-    function userTwo() {
-        let myNumber = Math.floor((Math.random() * 2) + 1);
-
-        let move = prompt("Am I thinking of 1 or 2?");
-        if(move == myNumber){
-            alert("Correct!");
-            currentDistanceofUserTwo += 100;
-            ctxTwo.clearRect(0, 0, canvasTwo.width, canvasTwo.height);
-            drawCircleTwo(currentDistanceofUserTwo);
-        } else {
-            alert("Wrong! Maybe next time. The number was " + myNumber);
-        }
+    drawCircle() {
+	    this.ctx.beginPath();
+	    this.ctx.arc(this.distance, 50, 50, 0, 2 * Math.PI);
+	    this.ctx.fillStyle = "red";
+	    this.ctx.fill();
+	    this.ctx.stroke();
     }
 
+    trivia(myNumber) {
+        let move = prompt("Am I thinking of 1 or 2?");
+
+        if (move == myNumber) {
+            alert("Correct!");
+            this.distance += 100;
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.drawCircle(this.distance);
+
+            if (this.distance == 950) {
+                this.win = true;
+            }
+        } else {
+            alert("Wrong! Maybe next time.  The number was " + myNumber);
+        }
+    }
+}
+
+function game() {
     let roundNum = 0;
 
-    while (true){
+    let userOne = new User(50, document.getElementById("myCanvasOne"));
+    let userTwo = new User(50, document.getElementById("myCanvasTwo"));
 
-        if (currentDistanceofUserOne == 950) {
-            alert("User One Wins!!!");
-            break;
-        } else if (currentDistanceofUserTwo == 950) {
-            alert("User Two Wins!!!");
-            break;
+    userOne.drawCircle();
+    userTwo.drawCircle();
+    
+    while (userOne.win === false && userTwo.win === false) {
+        let randomNumber = Math.floor((Math.random() * 2) + 1);
+
+        if (roundNum % 2 === 0) {
+            userOne.trivia(randomNumber);
         } else {
-            if (roundNum % 2 == 0) {
-            	userOne();
-            } else {
-            	userTwo();
-            } 
-        }
+            userTwo.trivia(randomNumber);
+        } 
+
         roundNum++;
     }
 
-    alert("You win!!!");
+    if (userOne.win) {
+        alert("User One Wins!!!");
+    }
+
+    if (userTwo.win) {
+        alert("User Two Wins!!!");
+    }
 }
 
-turn();
+game();
